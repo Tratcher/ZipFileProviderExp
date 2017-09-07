0d9bc2c04dd6538f6490d5b135b9cbf75b42426a
Chris R <Tratcher@outlook.com>
2017-09-06 22:18:14 -0700

    Add the ZipFileProvider and test console app.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using FileProviders.Zip;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var provider = new ZipFileProvider("static.zip");
+            var fileInfo = provider.GetFileInfo("/favicon.ico");
+            Console.WriteLine(fileInfo.Name);
+        }
+    }
+}
---
+++ FileProviders.Zip/ZipFileProvider.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Primitives;
+
+namespace FileProviders.Zip
+{
+    public class ZipFileProvider : IFileProvider
+    {
+        private readonly ZipArchive _archive;
+
+        public ZipFileProvider(string path)
+        {
+            _archive = ZipFile.OpenRead(path);
+        }
+
+        public IDirectoryContents GetDirectoryContents(string subpath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IFileInfo GetFileInfo(string subpath)
+        {
+            // internal paths do not start with a root slash.
+            var entry = _archive.GetEntry(subpath.Substring(1));
+            if (entry == null)
+            {
+                return new NotFoundFileInfo(subpath);
+            }
+
+            return new ZippedFileInfo(entry);
+        }
+
+        public IChangeToken Watch(string filter)
+        {
+            // Assume Zips don't change.
+            return NullChangeToken.Singleton;
+        }
+
+        private class ZippedFileInfo : IFileInfo
+        {
+            private ZipArchiveEntry _entry;
+
+            public ZippedFileInfo(ZipArchiveEntry entry)
+            {
+                _entry = entry;
+            }
+
+            public bool Exists => true;
+
+            public long Length => _entry.Length;
+
+            public string PhysicalPath => null;
+
+            public string Name => _entry.Name; // TODO: Dir names
+
+            public DateTimeOffset LastModified => _entry.LastWriteTime;
+
+            public bool IsDirectory => false; // TODO: Based on the file name ending in slash
+
+            public Stream CreateReadStream()
+            {
+                return _entry.Open();
+            }
+        }
+    }
+}
